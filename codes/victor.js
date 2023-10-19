f2dbdd74e7a165a3924b6cb9634ee46dca4d6a05
kingwuzy <vj77782@gmail.com>
2023-10-19 11:03:57 -0700

    wuzy cars

---
+++ codes/victor.js
@@ -0,0 +1,69 @@
+// // // let num=1
+// // // while(num<=100){
+// // //     if (num%3===0){
+// // //         console.log("FIZZBUZ")
+ 
+// // //     }
+// // //     else if (num%5===0){
+
+// // //         console.log("BUZZ")
+// // //     }
+// // //      else if (num%3===0){
+
+// // //         console.log("FUZZ")
+    
+// // //      }
+// // //      else{
+// // //          console.log(num)
+// // //     //  }//
+
+// // let picked=document.getElementById("greet")
+// // console.log(picked)
+// // picked.innerText="how are you"
+// // picked.style.backgroundColor="pink"
+// // picked.style.fontSize="50px"
+// let wuzy=document.getElementsByClassName("be")
+// console.log(wuzy)
+
+// let tags=document.getElementsByTagName("p")
+// console.log(tags)
+
+// let vicor=document.getElementsByTagName("span")
+// console.log(vicor)
+
+
+// let john=document.querySelector("span")
+// console.log(john)
+// let james=document.querySelector("p")
+// console.log(james)
+
+// let kelvin=document.querySelectorAll("span")
+// console.log(kelvin)
+
+// let kelvin = document.getElementById("p")
+// let btn = document.querySelector("button")
+// let p2=document.querySelector("#text")
+// btn.addEventListener("click",victor)
+// function victor(){
+//     kelvin.innerText="Hello victor"
+//     btn.innerText="changed"
+//     p2.innerText="kelvin was changed to victor"
+    
+
+// }
+
+// let ayo=document.getElementById("a")
+// console.log(ayo);
+// function ayo(){
+//     let wrapper=document.getElementsByClassName("wrapper")
+    
+
+
+// }
+
+
+
+
+
+
+
